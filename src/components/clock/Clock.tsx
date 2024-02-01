8765d6070e007e5321548a6c069527dd0c75a170
rrana011993 <89764057+rrana011993@users.noreply.github.com>
2024-02-01 15:34:29 +0530

    Update Clock.tsx

--- src/components/clock/Clock.tsx
+++ src/components/clock/Clock.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import './Clock.css'
 
 interface props {
@@ -28,29 +28,22 @@ export default function Clock({ timeZone, dominant, selectedDuration }: props) {
 
     const [hour, minute, second] = time.split(/:| /);
 
-    const hourStyle = {
-        transform: `rotate(${(parseInt(hour) % 12 / 12 + parseInt(minute) / 60 / 12) * 360}deg)`,
-    };
+    const hourStyle = useMemo(() => { return { transform: `rotate(${(parseInt(hour) % 12 / 12 + parseInt(minute) / 60 / 12) * 360}deg)` } }, [hour, minute]);
 
-    const minuteStyle = {
-        transform: `rotate(${(parseInt(minute) / 60) * 360}deg)`,
-    };
+    const minuteStyle = useMemo(() => { return { transform: `rotate(${(parseInt(minute) / 60) * 360}deg)` } }, [minute])
 
-    const secondStyle = {
-        transform: `rotate(${(parseInt(second) / 60) * 360}deg)`
-    };
+    const secondStyle = useMemo(() => { return { transform: `rotate(${(parseInt(second) / 60) * 360}deg)` } }, [second])
 
     const getDayAndDate = (timezone: string) => {
         const now = new Date();
         const options: any = { timeZone: timezone, weekday: "short", day: 'numeric', month: 'short' };
         const formatter = new Intl.DateTimeFormat('en-US', options);
-
         const formattedDate = formatter.format(now);
-
         return formattedDate;
     }
 
-    const [day, date] = getDayAndDate(timeZone).split(',')
+    const [weekDay, date] = getDayAndDate(timeZone).split(',')
+    const [month, day] = date.trim().split(' ')
 
     return <div className={`dial ${alertMsg && 'dial-alert'} ${dominant && 'dial-dominant'} ${dominant && alertMsg && 'dial-alert-dominant'}`}>
         <div className={`inner-dial-circle ${dominant && 'inner-dial-circle-dominant'}`}></div>
@@ -66,9 +59,9 @@ export default function Clock({ timeZone, dominant, selectedDuration }: props) {
         <div className={`minute-hand ${dominant && 'minute-hand-dominant'}`} style={minuteStyle}></div>
         <div className='second-hand' style={secondStyle}></div>
         <div className={`center-dot ${dominant && 'center-dot-dominant'}`}></div>
-        <div className={`inner-circle day ${dominant && 'day-dominant'}`}>{day}</div>
+        <div className={`inner-circle day ${dominant && 'day-dominant'}`}>{weekDay}</div>
         <div className={`inner-circle date ${dominant && 'date-dominant'}`}>
-            <span>{date.trim().split(' ')[1]}</span> {date.trim().split(' ')[0]}
+            <span>{day}</span> {month}
         </div>
         <div className=' inner-circle am-pm'>{time.split(' ')[1]}</div>
         {alertMsg && (
